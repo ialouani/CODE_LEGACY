88dc4c1dc4db5bbdf1e195dfcee9569ef00f5ab0
ALOUANI Ismail <Ismail.Alouani@bordeaux-inp.fr>
2022-05-15 02:39:01 +0100

    code_legacy 100%  ./////////

---
+++ two/build_for_install/CMakeFiles/hash_dyn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hash_dyn.dir/src/hash.c.o"
+  "libhash_dyn.pdb"
+  "libhash_dyn.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/hash_dyn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ two/build_for_install/CMakeFiles/hash_sta.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hash_sta.dir/src/hash.c.o"
+  "libhash_sta.pdb"
+  "libhash_sta.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/hash_sta.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
